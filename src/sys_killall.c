5862f4cc9c54755ee49a3e031ec1e26428604b94
ZanissNguyen <phat.nguyenthanh2408@gmail.com>
2025-04-10 21:27:20 +0700

    Systemcall update

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -1,48 +1,113 @@
 /*
- * Copyright (C) 2025 pdnguyen of HCMC University of Technology VNU-HCM
- */
+* Copyright (C) 2025 pdnguyen of HCMC University of Technology VNU-HCM
+*/
 
 /* Sierra release
- * Source Code License Grant: The authors hereby grant to Licensee
- * personal permission to use and modify the Licensed Source Code
- * for the sole purpose of studying while attending the course CO2018.
- */
+* Source Code License Grant: The authors hereby grant to Licensee
+* personal permission to use and modify the Licensed Source Code
+* for the sole purpose of studying while attending the course CO2018.
+*/
+
+#include <string.h>
 
 #include "common.h"
 #include "syscall.h"
 #include "stdio.h"
 #include "libmem.h"
+#include "queue.h"
+
+
+void get_name(struct pcb_t * caller, char * result)
+{
+    //init
+    char buffer[33];
+    sprintf(buffer, "%d", caller->pid);
+
+    // ex:
+    // process id = 1
+    // result = "P1"
+    result[0] = 'P';
+    int i = 0;
+    while (buffer[i]!='\0')
+    {
+        result[i+1] = buffer[i];
+        i++;
+    }
+    result[i] = '\0';
+}
 
 int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 {
-    char proc_name[100];
+    // init
+    char to_kill_proc_name[100];
     uint32_t data;
-
-    //hardcode for demo only
     uint32_t memrg = regs->a1;
     
-    /* TODO: Get name of the target proc */
-    //proc_name = libread..
+    // Get to kill process name, read from memory region
     int i = 0;
     data = 0;
     while(data != -1){
         libread(caller, memrg, i, &data);
-        proc_name[i]= data;
-        if(data == -1) proc_name[i]='\0';
+        to_kill_proc_name[i]= data;
+        if(data == -1) to_kill_proc_name[i]='\0';
         i++;
     }
-    printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
+    printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, to_kill_proc_name);
 
-    /* TODO: Traverse proclist to terminate the proc
-     *       stcmp to check the process match proc_name
-     */
-    //caller->running_list
-    //caller->mlq_ready_queu
+    // traverse the running list and terminate matching
+    struct queue_t *run_q = caller->running_list;
+    for (int j = 0; j < run_q->size; j++) 
+    {
+        char name[100];
+        get_name(run_q->proc[j], name);
+        if (strcmp(name, to_kill_proc_name) == 0) // matching logic
+        { 
+            libfree(run_q->proc[j], memrg);
+            for (int k = j; k < run_q->size - 1; k++) 
+            {
+                run_q->proc[k] = run_q->proc[k + 1];
+            }
+            run_q->size--;
+            j--;
+        }
+    }
 
-    /* TODO Maching and terminating 
-     *       all processes with given
-     *        name in var proc_name
-     */
+#ifdef MLQ_SCHED
+    struct queue_t * mlq_ready_queue = caller->mlq_ready_queue;
+    for (int prio = 0; prio < MAX_PRIO; prio++) 
+    {
+        struct queue_t * queue = &mlq_ready_queue[prio];
+        for (int j = 0; j < queue->size; j++) 
+        {
+            char name[100];
+            get_name(queue->proc[j], name);
+            if (strcmp(name, to_kill_proc_name) == 0) {
+                libfree(queue->proc[j], memrg);
+                for (int k = j; k < queue->size - 1; k++) {
+                    queue->proc[k] = queue->proc[k + 1];
+                }
+                queue->size--;
+                j--;
+            }
+        }
+    }
+#endif  
+#ifndef MLQ_SCHED
+    struct queue_t * ready_queue = caller->ready_queue;
+    for (int j = 0; j < ready_queue->size; j++) 
+    {
+        char name[100];
+        get_name(ready_queue->proc[j], name);
+        if (strcmp(name, to_kill_proc_name) == 0) { // matching
+            libfree(ready_queue->proc[j], memrg);
+            for (int k = j; k < ready_queue->size - 1; k++) {
+                ready_queue->proc[k] = ready_queue->proc[k + 1];
+            }
+            ready_queue->size--;
+            j--;
+        }
+    }
+#endif
 
     return 0; 
 }
--- src/sys_xxxhandler.c
+++ src/sys_xxxhandler.c
@@ -3,8 +3,30 @@
 #include "syscall.h"
 #include "stdio.h"
 
+void get_name(struct pcb_t * caller, char * result)
+{
+    //init
+    char buffer[33];
+    sprintf(buffer, "%d", caller->pid);
+
+    // ex:
+    // process id = 1
+    // result = "P1"
+    result[0] = 'P';
+    int i = 0;
+    while (buffer[i]!='\0')
+    {
+        result[i+1] = buffer[i];
+        i++;
+    }
+    result[i] = '\0';
+}
+
 int __sys_xxxhandler(struct pcb_t * caller, struct sc_regs * regs)
 {
+    char name[100];
+    get_name(caller, name);
+    printf("%s\n", name);
     /* TODO: implement syscall job */
     printf("The first system call parameter %d\n", regs->a1);
     return 0;
